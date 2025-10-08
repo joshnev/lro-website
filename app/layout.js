468e385d408cfbea86c91818be02ded03cecc1a7
joshnev <joshnevard@gmail.com>
2025-10-08 15:34:39 +0100

    removed icon code from meta data

--- app/layout.js
+++ app/layout.js
@@ -3,9 +3,6 @@ import "./globals.css";
 export const metadata = {
   title: "LRO Contractors",
   description: "UK-based shopfitting and construction specialists.",
-  icons: {
-    icon: "/icon.ico",
-  },
 };
 
 export default function RootLayout({ children }) {
